85b61a3f4f544ecd4392c63eb6e8eec4afb557a7
Baldy76 <rgleech76@gmail.com>
2026-03-30 11:46:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'holiday-planner-v1.0.1';
+const CACHE_NAME = 'holiday-planner-v1.0.2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -7,17 +7,15 @@ const ASSETS_TO_CACHE = [
     './manifest.json'
 ];
 
-// Install Event: Caches the app files
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(ASSETS_TO_CACHE);
         })
     );
-    self.skipWaiting(); // Forces the new service worker to activate immediately
+    self.skipWaiting(); 
 });
 
-// Activate Event: Cleans up old caches when you update the version number
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(keys => {
@@ -29,9 +27,7 @@ self.addEventListener('activate', event => {
     self.clients.claim();
 });
 
-// Fetch Event: Serves files from cache, EXCEPT for your Google Sheet
 self.addEventListener('fetch', event => {
-    // We strictly ignore the Google Sheets URL so it always fetches live data
     if (event.request.url.includes('docs.google.com')) {
         return; 
     }
