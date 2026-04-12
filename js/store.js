154bb03dd3cd84e02b8ca6169df4dad65357e97f
Baldy76 <rgleech76@gmail.com>
2026-04-12 18:58:48 +0100

    Update store.js

--- js/store.js
+++ js/store.js
@@ -48,7 +48,7 @@ export const getVal = async (key) => {
 // UTILITIES
 export const escapeHTML = (str) => {
     if (!str) return "";
-    return String(str).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;"); 
+    return String(str).replace(/&/g, "&").replace(/</g, "<").replace(/>/g, ">").replace(/"/g, """).replace(/'/g, "'"); 
 };
 
 export const parseDateTime = (dateStr, timeStr = '') => {
