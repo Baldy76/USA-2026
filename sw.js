7b8e24b758443957b63df6fcda17934ae3e48ca2
Baldy76 <rgleech76@gmail.com>
2026-04-15 13:50:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = 'holiday-planner-v2.1.35';
+const CACHE_NAME = 'holiday-planner-v2.1.36';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
-    './style.css?v=2.1.35',
-    './js/main.js?v=2.1.35',
+    './style.css?v=2.1.36',
+    './js/main.js?v=2.1.36',
     './js/store.js?v=2.1.31',
     './js/api.js?v=2.1.32',
-    './js/ui.js?v=2.1.35',
+    './js/ui.js?v=2.1.36',
     './manifest.json',
     './img/la.jpg',
     './img/utah.jpg',
