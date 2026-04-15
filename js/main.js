d025881c8a7a27293b0417801c54afd1b6a03c44
Baldy76 <rgleech76@gmail.com>
2026-04-16 00:12:31 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { state, setVal, getVal, parseDateTime } from './store.js';
 import { loadAllData, initLiveCurrency, preCacheImages, syncToCloud } from './api.js';
 
 import { 
-    applyTheme, setThemeMode, updateMetaThemeColor, updateTimeAndCountdown, updateGreeting, saveTripSettings,
+    applyTheme, setThemeMode, updateMetaThemeColor, updateTimeAndCountdown, saveTripSettings, updateGreeting,
     convertCurrency, setTip, calculateTip, populateDropdown, clearCustomFamilies, updateFamilyFilter,
     initWeatherPill, setWeatherCity, openWeatherModal, closeWeatherModal,
     renderItinerary, renderTravelVault, renderAccommodations, handleFileUpload, renderWallet,
