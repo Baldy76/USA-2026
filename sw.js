6ed013d44845ea280f467008fff0186cceec79d1
Baldy76 <rgleech76@gmail.com>
2026-04-28 15:33:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,11 @@
-const CACHE_NAME = 'planner-v9-5';
+const CACHE_NAME = 'planner-v9-6';
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './js/main.js',
-  './js/store.js',
-  './js/api.js',
-  './js/ui.js',
-  './js/core/clock.js',
-  './js/core/theme.js',
-  './js/core/animations.js',
-  './js/features/itinerary.js',
-  './js/features/travel.js',
-  './js/features/guides.js',
-  './js/features/quotes.js',
-  './js/features/wallet.js',
-  './js/features/briefing.js',
-  './js/features/tools.js',
-  './js/features/weather.js',
-  './js/features/roulette.js',
-  './js/features/meetup.js',
-  './js/features/checklist.js'
+  './', './index.html', './style.css', './js/main.js', './js/store.js', './js/api.js', './js/ui.js',
+  './js/core/clock.js', './js/core/theme.js', './js/core/animations.js',
+  './js/features/itinerary.js', './js/features/travel.js', './js/features/guides.js',
+  './js/features/quotes.js', './js/features/weather.js', './js/features/briefing.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))).then(() => self.clients.claim()));
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)));
-});
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => k !== CACHE_NAME && caches.delete(k))))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
