6ec35bc964d908063513f754294315d16adc2732
Baldy76 <rgleech76@gmail.com>
2026-04-28 15:23:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,60 @@
-const CACHE_NAME = 'holiday-planner-v7.8.0';
-const ASSETS_TO_CACHE = [
-    './',
-    './index.html',
-    './style.css',
-    './js/main.js',
-    './js/store.js',
-    './js/api.js',
-    './js/ui.js',
-    './js/features/tools.js',
-    './js/features/roulette.js',
-    './js/features/meetup.js',
-    './js/features/weather.js',
-    './js/features/checklist.js',
-    './manifest.json',
-    './img/la.jpg',
-    './img/utah.jpg',
-    './img/vegas.jpg',
-    './img/flights.jpg',
-    './img/bg.jpg',
-    './img/logo.png',
-    './img/icon-192.png'
+const CACHE_NAME = 'planner-v9-4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './img/icon-192.png',
+  './img/icon-512.png',
+  './img/la.jpg',
+  './img/utah.jpg',
+  './img/vegas.jpg',
+  './img/flights.jpg',
+  './js/main.js',
+  './js/store.js',
+  './js/api.js',
+  './js/ui.js',
+  './js/core/clock.js',
+  './js/core/theme.js',
+  './js/core/animations.js',
+  './js/features/itinerary.js',
+  './js/features/travel.js',
+  './js/features/guides.js',
+  './js/features/quotes.js',
+  './js/features/wallet.js',
+  './js/features/briefing.js',
+  './js/features/tools.js',
+  './js/features/weather.js',
+  './js/features/roulette.js',
+  './js/features/meetup.js',
+  './js/features/checklist.js'
 ];
 
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => {
-    event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
-    self.clients.claim();
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
-    if (event.request.url.includes('docs.google.com') || event.request.url.includes('openweathermap.org') || event.request.url.includes('frankfurter.dev')) return;
-    event.respondWith(fetch(event.request).then(response => {
-        const rc = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, rc));
-        return response;
-    }).catch(() => caches.match(event.request)));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
