23326b3838f70d8cb75eb7fa9de49414dd3b3493
Baldy76 <rgleech76@gmail.com>
2026-07-06 15:58:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'holiday-planner-v9.2.0'; // Bumping this forces all phones to update!
+const CACHE_NAME = 'holiday-planner-v9.3.0'; // Bumping this forces all phones to update!
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
